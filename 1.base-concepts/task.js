91076c37d48123ce49da218fd73eb1195e4fafa9
Дмитрий Дрожжин <diman161rus.diman@yandex.ru>
2022-07-14 14:29:47 +0300

    Work Diskriminant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,24 @@
+"use sctrict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+
+  let d = b**2 - 4 * a * c;
+  let rootOne;
+  let rootTwo;
+  if (d > 0) {
+    rootOne = (-b + Math.sqrt(d)) / (2 * a);
+    rootTwo = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [rootOne, rootTwo];
+  } else if (d === 0) {
+    rootOne = -b / (2 * a);
+    arr = [rootOne];
+  } else if (d < 0) {
+    arr = [];
+    
+  }
+
   return arr; // array
 }
 
